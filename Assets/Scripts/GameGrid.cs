cd5dfaaf1fc1ccd8910b5a4c449b52656a6191fc
Eemeli Leppä <48524030+JustAGoldeneye@users.noreply.github.com>
2021-06-04 22:14:02 +0300

    Added code for moving Unit, resturctured plans

--- Assets/Scripts/GameGrid.cs
+++ Assets/Scripts/GameGrid.cs
@@ -2,24 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameGrid : MonoBehaviour // POISTA MonoBehavior, kun/jos saat tämän luokan Battle alaiseksi 
+public class GameGrid : MonoBehaviour 
 {
-    public Unit[] UnitList;
-    public Unit[,] UnitTable;
+    public float CellSize = 1f;
+    [HideInInspector] public List<UnitControl> PlayerUnitList;
+    [HideInInspector] public List<UnitControl> EnemyUnitList;
+    public UnitControl[,] UnitTable;
 
-    public void Awake() // MUUTA ehkä manuaalisesti ajattevaksi setupiksi, kun/jos saat tämän luokan Battle alaiseksi; PITÄISIKÖ olla Awake vai Start vai molemmat?
+    public void Awake() // PITÄISIKÖ olla Awake vai Start vai molemmat?
     {
-        UnitTable = new Unit[2,5];
-        PrintUnitTable();
+        // TODO
+    }
+
+    public float ConvertGridPosToRealPos(int a)
+    {
+        return a * CellSize + 0.5f;
     }
 
     // DEBUGGING
-    public void PrintUnitList()
+    public void PrintPlayerUnitList()
     {
         string row = "";
-        for (int i = 0; i < UnitList.Length; i++)
+        for (int i = 0; i < PlayerUnitList.Count; i++)
         {
-            row = (row + UnitList[i] + ", ");
+            row = (row + PlayerUnitList[i] + ", ");
+        }
+        Debug.Log(row);
+    }
+
+    // DEBUGGING
+    public void PrintenemyUnitList()
+    {
+        string row = "";
+        for (int i = 0; i < EnemyUnitList.Count; i++)
+        {
+            row = (row + EnemyUnitList[i] + ", ");
         }
         Debug.Log(row);
     }
---
+++ Assets/Scripts/UnitControl.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitControl : MonoBehaviour
+{
+    public float MoveSpeed;
+    public GameGrid GameGrid;
+
+    private float TargetPosX;
+    private float TargetPosY;
+
+    public void Awake()
+    {
+        TargetPosX = transform.position.x;
+        TargetPosY = transform.position.y;
+    }
+
+    public void MoveToGridPos(int x, int y)
+    {
+        TargetPosX = GameGrid.ConvertGridPosToRealPos(x);
+        TargetPosY = GameGrid.ConvertGridPosToRealPos(y);
+
+        // TODO: Läheteä tieto Gridille
+    }
+
+    private bool IsInTargetPos()
+    {
+        return transform.position.x == TargetPosX && transform.position.y == TargetPosY;
+    }
+
+    public void FixedUpdate()
+    {
+        if (!IsInTargetPos())
+        {
+            MoveTowardsTargetPos();
+        }
+    }
+
+    private void MoveTowardsTargetPos()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(TargetPosX, TargetPosY), Time.deltaTime * MoveSpeed);
+    }
+}
